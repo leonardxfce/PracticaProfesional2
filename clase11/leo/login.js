07565abd76714f403cdb2b102d4157728866935c
leonardxfce <leonardo.araoz.dev@gmail.com>
2018-06-22 11:07:58 -0300

    agrego un comentario al codigo jquery y actualizo el index

--- clase11/leo/login.js
+++ clase11/leo/login.js
@@ -1,5 +1,8 @@
-window.onload = incio;
+// con esta linea le decimos que funcion 
+// debe ejecutar al cargar (load) la pagina
+window.onload = incio; 
 
+// "equivalente" a public static void main
 function incio() {
     $(".card").slideDown();
 }
