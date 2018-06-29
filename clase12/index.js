de0fba5358edfce58bdaba3959d5f5006c8f5dc4
leonardxfce <leonardo.araoz.dev@gmail.com>
2018-06-29 10:46:26 -0300

    presentacion

---
+++ clase12/index.js
@@ -0,0 +1,18 @@
+Reveal.initialize({
+    // Display controls in the bottom right corner
+    controls: true,
+    // Display a presentation progress bar
+    progress: true,
+    // If true; each slide will be pushed to the browser history
+    history: true,
+    // Loops the presentation, defaults to false
+    loop: false,
+    // Flags if mouse wheel navigation should be enabled
+    mouseWheel: true,
+    // Apply a 3D roll to links on hover
+    rollingLinks: true,
+    // UI style
+    theme: Reveal.getQueryHash().theme || 'default',
+    // Transition style
+    transition: Reveal.getQueryHash().transition || 'default'
+});
