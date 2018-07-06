f5f004756c8a242b808eb69d016d252cb629cf5a
andreahoran <caro_barroso1998@hotmail.com>
2018-07-06 18:06:16 -0300

    cambios

--- clase12/leo-test/login.js
+++
@@ -1,9 +0,0 @@
-// con esta linea le decimos que funcion 
-// debe ejecutar al cargar (load) la pagina
-window.onload = incio;
-
-// "equivalente" a public static void main
-function incio() {
-    const carta = new jQuery(".card");
-    carta.slideDown();
-}
