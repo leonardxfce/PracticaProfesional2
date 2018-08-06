874f0623a173ca4afd6ce2c439b097d63980c752
TheBoudica <theboudica98@gmail.com>
2018-08-06 19:18:37 -0300

    año visiesto

--- clase13/Santiago/main.js
+++ clase13/Santiago/main.js
@@ -1,13 +1,18 @@
-function FizzBazz(a){
-    if(a%3 == 0 ){
-        if(a%5 == 0){
-            return "FizzBuzz"
-        } else {
-            return "Fizz"
-        }
-    } else {
-        if(a%5 == 0){
-            return "Buzz"
-        }
+function FizzBazz(a) {
+    if (a % 3 == 0 && a % 5 == 0) {
+        return "FizzBuzz"
     }
+    if (a % 5 == 0) {
+        return "Buzz"
+    }
+    if (a % 3 == 0) {
+        return "Fizz"
+    }
+}
+
+function anioBisiesto(a){ 
+    if (a % 4 == 0 && a % 400 == 0) {
+        return true
+    }
+    return false
 }
--- clase13/Santiago/text.js
+++ clase13/Santiago/text.js
@@ -1,16 +1,28 @@
-function probarFizz(assert){
-    var resultado = FizzBazz (3)
+function probarFizz(assert) {
+    var resultado = FizzBazz(3)
     assert.equal("Fizz", resultado);
-    resultado = FizzBazz (5)
+    resultado = FizzBazz(5)
     assert.equal("Buzz", resultado)
-    resultado = FizzBazz (6)
+    resultado = FizzBazz(6)
     assert.equal("Fizz", resultado)
-    resultado = FizzBazz (30)
+    resultado = FizzBazz(30)
     assert.equal("FizzBuzz", resultado)
-    resultado = FizzBazz (35)
+    resultado = FizzBazz(35)
     assert.equal("Buzz", resultado)
-    resultado = FizzBazz (15)
+    resultado = FizzBazz(15)
     assert.equal("FizzBuzz", resultado)
 }
 
-QUnit.test("probando fizzBuzz",probarFizz);
+QUnit.test("probando fizzBuzz", probarFizz);
+
+function probarAnioBisiesto(assert) {
+    var resultado = anioBisiesto(1998)
+    assert.equal(false, resultado)
+    resultado = anioBisiesto(2000)
+    assert.equal(true, resultado)
+    resultado = anioBisiesto(218)
+    assert.equal(false, resultado)
+    resultado = anioBisiesto(1999)
+    assert.equal(false, resultado)
+}
+QUnit.test("probando anioBisiesto", probarAnioBisiesto);
